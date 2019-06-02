5d8e3c362db06555961af78ec02a2a784966ff81
da-evasechko <evasechko@darwinapps.net>
2019-06-02 22:07:07 +0300

    some technical changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,90 @@
 // Модуль представляет из себя переменную, которой присвоено значение самовызывающейся анонимной функции
 // Функция возвращает объект, предоставляющий публичный API для работы с модулем
 
-
-import './modules/call-us.js'
-import './modules/sell-unit.js'
 import $ from 'jquery'
 import { TSCallSignatureDeclaration } from 'babel-types';
-
 window.$ = $
+window.addEventListener('DOMContentLoaded', function() {
+    //variables for modal of sell-unit
+    var closeModal = document.querySelector('.close-modal');
+    var previousButton = document.querySelector('.previous');
+    var nextButton = document.querySelector('.next');
+    var slider = document.querySelector('.carousel');
+    var slyderElems = slider.querySelectorAll('img');
+    var visible = slyderElems[0];
+    var sellUnit = document.querySelector('.sell-unit');
+    var readMore = document.querySelectorAll('.read-more');
+    var callUsBtn = document.querySelector('#call-us');
+    //Variables for call-us form
+    var callUsForm = document.querySelector('#call-us-form');
+    var callUsField = document.querySelectorAll('input');
+    var callUsLabel = document.querySelectorAll('label');
+    var callUsClose = document.querySelector('.close');
+    var indexPage = document.querySelector('body');
+    var scrolled;
+    var i;
 
+    window.onscroll = function() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    }
+
+
+// Script for sell-unit modal
+
+
+    nextButton.addEventListener('click', function(e){
+        if(visible.nextSibling === null) {
+            visible.style.display = 'none';
+            visible = slyderElems[0];
+            visible.style.display = 'block';
+        } else {
+            visible.style.display = 'none';
+            visible = visible.nextSibling;
+            visible.style.display = 'block';
+        }
+    })
+    previousButton.addEventListener('click', function(e){
+        if(visible.previousSibling === null) {
+            visible.style.display = 'none';
+            visible = slyderElems[2];
+            visible.style.display = 'block';
+        } else {
+            visible.style.display = 'none';
+            visible = visible.previousSibling;
+            visible.style.display = 'block';
+        }
+    })
+
+    for(var j=0; j<readMore.length; j++) {
+        readMore[j].addEventListener('click', function(e){
+            sellUnit.style.display = 'block';
+            sellUnit.style.top = scrolled + 20 + 'px';
+        })
+    }
+    
+    closeModal.addEventListener('click', function(e){
+        sellUnit.style.display = 'none';
+    })
+
+
+// Script for call-us form
+
+
+    callUsBtn.addEventListener('click', function(e) {
+        callUsForm.style.display = 'block';
+        callUsForm.style.top = scrolled + 100 + 'px';
+        document.body.style.overflow = "hidden";
+    });
+
+    callUsClose.addEventListener('click', function(e) {
+        callUsForm.style.display = 'none';
+        document.body.style.overflow = "";
+    });
+
+    for (var i = 0; i < callUsField.length; i++) {
+        callUsField[i].addEventListener('focus', function(){
+            this.nextElementSibling.classList.add('focus');
+        })
+    }
+    
+});
